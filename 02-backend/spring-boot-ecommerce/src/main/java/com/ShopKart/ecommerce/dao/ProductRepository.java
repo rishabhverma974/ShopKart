4d7b857674eef8382a0bf0e83580eefa1d0a8c27
Rishabh Verma <rishabhverma947@gmail.com>
2025-07-04 21:47:19 +0530

    v2- Cross Origin

--- 02-backend/spring-boot-ecommerce/src/main/java/com/ShopKart/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/ShopKart/ecommerce/dao/ProductRepository.java
@@ -3,6 +3,8 @@ package com.ShopKart.ecommerce.dao;
 
 import com.ShopKart.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
