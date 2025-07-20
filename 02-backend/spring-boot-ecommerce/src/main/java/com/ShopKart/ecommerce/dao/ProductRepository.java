b37f8e30a47d164ac0c4614f61aa0a69c924be18
Rishabh Verma <rishabhverma947@gmail.com>
2025-07-20 18:05:09 +0530

    v2- category search

--- 02-backend/spring-boot-ecommerce/src/main/java/com/ShopKart/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/ShopKart/ecommerce/dao/ProductRepository.java
@@ -2,9 +2,17 @@ package com.ShopKart.ecommerce.dao;
 
 
 import com.ShopKart.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    //BTS Select * Fromm product where category_id = ?
+    //URL Spring will automatically give endpoints...
+    //.../products/search/findByCategoryId?id=2
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
